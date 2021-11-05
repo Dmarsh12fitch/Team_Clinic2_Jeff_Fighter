52751ed6189b9c30c28abf8fc8b5b75813040580
David Marshall <dmarsh12@student.fitchburgstate.edu>
2021-11-05 19:48:47 -0400

    input and player manager update

--- Assets/Scripts/newScripts/InputManager.cs
+++ Assets/Scripts/newScripts/InputManager.cs
@@ -27,10 +27,93 @@ public class InputManager : MonoBehaviour
     //checks for all the keys pressed
     void Update()
     {
+        //PLAYER1 INPUTS BEGIN -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+
+        //get MoveBackwards
         if (Input.GetKeyDown(KeyCode.A))
         {
-            Player1And2Manager.Instance.aControlKeyIsDown(Player1And2Manager.playerActionType.MoveBackwards);
+            //send a control key request to enable movebackwards
+            Player1And2Manager.Instance.controlKeyDown(1, Player1And2Manager.playerActionType.MoveBackwards);
+        } else
+        {
+            //send a control key request to disable movebackwards
+            Player1And2Manager.Instance.controlKeyUp(1, Player1And2Manager.playerActionType.MoveBackwards);
+
+
+            //Get MoveForwards
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                //send a control key request to enable moveforwards
+                Player1And2Manager.Instance.controlKeyDown(1, Player1And2Manager.playerActionType.MoveForwards);
+            } else
+            {
+                //send a control key request to disable moveforwards
+                Player1And2Manager.Instance.controlKeyUp(1, Player1And2Manager.playerActionType.MoveForwards);
+
+
+                //Get Block
+                if (Input.GetKeyDown(KeyCode.S))
+                {
+                    //send a control key request to enable block
+                    Player1And2Manager.Instance.controlKeyDown(1, Player1And2Manager.playerActionType.Block);
+                } else
+                {
+                    //send a control key request to disable block
+                    Player1And2Manager.Instance.controlKeyUp(1, Player1And2Manager.playerActionType.Block);
+
+
+                    //Get RegularAttack
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        //send a control key request to enable regular attack
+                        Player1And2Manager.Instance.controlKeyDown(1, Player1And2Manager.playerActionType.RegularAttack);
+                    } else
+                    {
+                        //no way to disable attack once launched
+
+
+                        //Get SuperAttack
+                        if (Input.GetKeyDown(KeyCode.Z))
+                        {
+                            //send a control key request to enable super attack
+                            Player1And2Manager.Instance.controlKeyDown(1, Player1And2Manager.playerActionType.SuperAttack);
+                            //no way to disable super attack once launched
+                        }
+                    }
+                }
+            }
         }
+
+
+        /*
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Player1And2Manager.Instance.controlKeyDown(1, Player1And2Manager.playerActionType.MoveForwards);
+        } else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Player1And2Manager.Instance.controlKeyDown(1, Player1And2Manager.playerActionType.Block);
+        } else
+        {
+            //state that the player should not be doing this anymore
+
+
+        }
+
+    */
+        //PLAYER1 INPUTS ENDSS -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+
+        /*
+        //player2 inputs
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Player1And2Manager.Instance.aControlKeyIsDown(2, Player1And2Manager.playerActionType.MoveForwards);
+        } else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Player1And2Manager.Instance.aControlKeyIsDown(2, Player1And2Manager.playerActionType.MoveBackwards);
+        }
+        */
     }
 
 
--- Assets/Scripts/newScripts/Player1And2Manager.cs
+++ Assets/Scripts/newScripts/Player1And2Manager.cs
@@ -38,12 +38,62 @@ public class Player1And2Manager : MonoBehaviour
     public playerActionType player2NextAction;
 
 
-    public void aControlKeyIsDown(playerActionType player1And2Input)
+    public void controlKeyDown(int whichPlayer, playerActionType player1And2Input)
     {
+        //see whether or not this action can be implemented and if so implement it
+        //Debug.Log("COMMDAND START : " + player1And2Input.ToString()); //test
+
+        if (whichPlayer == 1)
+        {
+            //for player1
+
+            //Make sure not in getting hit state
+            if (!player1CurrentAction.Equals(playerActionType.GotHit))
+            {
+                //For Launching Block, Super Attack, Regular Attack, MoveBackward, and MoveForward none of the first three can be active
+                if(!player1CurrentAction.Equals(playerActionType.Block) && !player1CurrentAction.Equals(playerActionType.RegularAttack) 
+                    && !player1CurrentAction.Equals(playerActionType.SuperAttack))
+                {
+                    //if the first three from above, then set input action to next action
+                    //if(player1And2Input.Equals(playerActionType.Block) || player)
+
+
+
+                }
+                
+
+
+            }
+        } else
+        {
+            //for player2
+
+
+        }
+
 
     }
 
+    public void controlKeyUp(int whichPlayer, playerActionType player1And2Input)
+    {
+        //see what should be done with the particular action and kill it (if it isn't already killed)
+        //Debug.Log("COMMDAND STOP : " + player1And2Input.ToString());  //test
+
+        if (whichPlayer == 1)
+        {
+            //for player1
+
+        }
+        else
+        {
+            //for player2
 
 
+        }
+    }
+
+
+    //make a function that makes the nextAction into the current action IF it allowed to by priority early, or after the current anim is happening if not
+    //maybe this shouldn't be called by update, but whenever a new input is called??? OR NOT..
 
 }
